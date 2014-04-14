2e3023b4e357ce6c46b6c457e48348d1d1316a8d
Travis Riggs <travis.a.riggs@gmail.com>
2014-04-14 14:58:37 -0400

    * Modified moving of files to operate safely

--- Chapter11/move_pictures.rb
+++ Chapter11/move_pictures.rb
@@ -4,8 +4,9 @@ this_dir = Dir.getwd
 data_dir = 'TestData'
 
 # Create a directory of test files
-
-Dir.mkdir(File.join(this_dir, data_dir))
+if !File.exists? File.join(this_dir, data_dir)
+  Dir.mkdir(File.join(this_dir, data_dir))
+end
 
 (1..12).each { |index|
   File.open(File.join(this_dir, data_dir, "Test-#{index}.jpg"), 'w') { |f|
@@ -19,7 +20,11 @@ batch_name = gets.chomp
 
 # Create new directory
 new_dir = File.join(this_dir, batch_name)
-Dir.mkdir(new_dir)
+
+if !File.exists? new_dir
+  Dir.mkdir(new_dir)
+end
+
 
 # Loop through files in directory and rename them
 Dir.foreach(File.join(this_dir, data_dir)) { |file|
@@ -38,8 +43,18 @@ Dir.foreach(File.join(this_dir, data_dir)) { |file|
     # Create new filename
     new_name = batch_name + "-" + number + extension
 
-    File.rename(File.join(this_dir, data_dir, file),
-      File.join(new_dir, new_name))
+    if File.exists? File.join(new_dir, new_name)
+
+      puts "WARNING! You already have a file by this name: #{new_name}"
+      exit
+
+    else
+
+      File.rename(File.join(this_dir, data_dir, file),
+        File.join(new_dir, new_name))
+
+    end
+
 
   end
 
