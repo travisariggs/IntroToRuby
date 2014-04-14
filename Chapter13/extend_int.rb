82984d468f0bb91c2113d63b29b14cb716315bda
Travis Riggs <travis.a.riggs@gmail.com>
2014-04-14 17:19:37 -0400

    * Added factorial method to Integer class

--- Chapter13/extend_int.rb
+++ Chapter13/extend_int.rb
@@ -1,4 +1,5 @@
 class Integer
+
   def to_eng
     if self == 5
       english = 'five'
@@ -7,7 +8,20 @@ class Integer
     end
     english
   end
+
+  def factorial
+    an_array = (1..self).to_a
+    factorial = 1
+    an_array.each { |f| factorial *= f }
+
+    factorial
+  end
+
 end
 
 puts 5.to_eng
 puts 42.to_eng
+
+puts
+puts 5.factorial
+
